4f88f303b4669292f2fcb119f0575811509e4eff
nombers <konsentik@gmail.com>
2022-10-16 13:37:31 +0300

    replace

---
+++ labs 1/task2 for.jl
@@ -0,0 +1,14 @@
+print("Введите k: ")
+k = parse(Int64, readline())
+f0 = 1
+f = 2
+t = 0
+for i=1:k
+    global t = f
+    global f = t + f0
+    global f0 = t
+    if f >= k
+        break
+    end
+end
+println("Первое число фибоначчи больше k ", f)
---
+++ labs 1/task2 while.jl
@@ -0,0 +1,11 @@
+print("Введите k: ")
+k = parse(Int64, readline())
+f0 = 1
+f = 2
+t = 0
+while f <= k
+    global t = f
+    global f = t + f0
+    global f0 = t
+end
+println("Первое число фибоначчи больше k ", f)
