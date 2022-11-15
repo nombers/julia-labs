62b2174e86cc2df26af711b2e350662b7953173d
nombers <konsentik@gmail.com>
2022-11-15 19:38:19 +0300

    help anysombady

--- labs 3/main.jl
+++ labs 3/main.jl
@@ -1,12 +1,23 @@
-include("modul.jl")
-import .replacing_elements_in_the_matrix
+module MyModul
 
+include("modul.jl")
+using Test
+using .replacing_elements_in_the_matrix
 
 print("Введите порядок матрицы: ")
 n = parse(Int8, readline())
-arr = replacing_elements_in_the_matrix.create_matrix(n)
-replacing_elements_in_the_matrix.output_matrix(arr)
+
+arr = create_matrix(n)
+output_matrix(arr)
+
 print("Введите иденксы каких элементов поменять в строке: ")
 k, l = [parse(Int8, x) for x in split(readline())]
-arr = replacing_elements_in_the_matrix.replacing_elements(arr, k, l, n)
-replacing_elements_in_the_matrix.output_matrix(arr)
+
+arr = replacing_elements(arr, k, l, n)
+output_matrix(arr) 
+
+@testset "Modul Test" begin
+    @test replacing_elements([[1, 2], [3, 4]], 1, 2, 2) == [[2, 1], [4, 3]]
+    @test replacing_elements([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 1, 3, 3) == [[3, 2, 1], [6, 5, 4], [9, 8, 7]]
+end
+end
--- labs 3/modul.jl
+++ labs 3/modul.jl
@@ -1,22 +1,23 @@
 module replacing_elements_in_the_matrix
 
-    function output_matrix(arr)
-        println("Получившаяся матрица: ")
-        for i in arr
-            println(i)
-        end
-        println()
-    end
+export  output_matrix, create_matrix, replacing_elements
 
-    function create_matrix(n)
-        return [[rand(10:99) for i in 1:n] for j in 1:n]
+function output_matrix(arr)
+    println("Получившаяся матрица: ")
+    for i in arr
+        println(i)
     end
+    println()
+end
 
-    function replacing_elements(arr, k, l, n)
-        for i in 1:n
-            arr[i][k], arr[i][l] = arr[i][l], arr[i][k]
-        end
-        return arr
+function create_matrix(n)
+    return [[rand(10:99) for i in 1:n] for j in 1:n]
+end
+
+function replacing_elements(arr, k, l, n)
+    for i in 1:n
+        arr[i][k], arr[i][l] = arr[i][l], arr[i][k]
     end
-
+    return arr
+end
 end
