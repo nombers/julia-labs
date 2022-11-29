e002d2b8fd86fe18769378f08a6adc10aabfa9ed
nombers <konsentik@gmail.com>
2022-11-29 20:09:37 +0300

    f5

--- labs 4/main.jl
+++ labs 4/main.jl
@@ -1,37 +1,14 @@
-function note(arr)
-    open("labs 4\\F.txt", "w") do f
-        for i in arr
-            write(f, i, "\n")
-        end
-    end
-end
+module Mainf
 
-function renote()
-    open("labs 4\\F.txt", "r") do old_file
-        text = readlines(old_file)
-        open("labs 4\\G.txt", "w") do new_file
-            for i in 1:length(text)
-                if i % 2 == 0
-                    write(new_file, text[i], "\n")
-                end
-            end
-        end
-    end
-end
+include("module.jl")
+using .Work_with_file
+using Test
 
-function input()
-    arr = []
-    while true
-        a = readline()
-        if a != ""
-            push!(arr, a)
-        else
-            break
-        end
-    end
-    return arr
+arr = input()
+write_file(arr)
+rewrite()
+    
+@testset "MyTest" begin
+    @test true == true 
 end
-
-input()
-note(arr)
-renote()
+end
---
+++ labs 4/module.jl
@@ -0,0 +1,45 @@
+module Work_with_file
+
+export input, write_file, read_file, rewrite
+
+function input()
+    arr = []
+    while true
+        a = readline()
+        if a != ""
+            push!(arr, a)
+        else
+            break
+        end
+    end
+    return arr
+end
+
+function write_file(arr)
+    open("labs 4\\F.txt", "w") do f
+        for i in arr
+            write(f, i, "\n")
+        end
+    end
+end
+
+function read_file(name_file)
+    open(name_file, "r") do old_file
+        global text
+        text = readlines(old_file)
+    end    
+    return text
+end
+
+function rewrite()
+    text = read_file("labs 4\\F.txt")
+    open("labs 4\\G.txt", "w") do new_file
+        for i in 1:length(text)
+            if i % 2 == 0
+                write(new_file, text[i], "\n")
+            end
+        end
+    end
+end
+
+end
