407eedbfa34fd0e95706bc28639cbf35406a113a
nombers <konsentik@gmail.com>
2023-01-12 21:50:36 +0300

    f

--- labs 4/main.jl
+++ labs 4/main.jl
@@ -2,10 +2,16 @@ module MyModule
 
 include("module.jl")
 using .Work_with_file
+using Test
 
-println("Введите данные файла:")
-arr = input()
-write_file(arr)
-rewrite()
-
+create_F()
+text = read_file("F.txt")
+result = rewrite(text)
+write_file(result, "G.txt")
+println(read_file("F.txt"))
+println(read_file("G.txt"))
+@testset "MyTest" begin
+    @test rewrite(["89", "16", "59", "49", "98", "38", "60", "64", "28"]) == ["16", "49", "38", "64"]
+    @test rewrite(["90", "81", "55", "43", "10", "48"]) == ["81", "43", "48"]
+end
 end
--- labs 4/module.jl
+++ labs 4/module.jl
@@ -1,45 +1,39 @@
 module Work_with_file
 
-export input, write_file, read_file, rewrite
+export create_F, write_file, read_file, rewrite
 
-function input()
-    arr = []
-    while true
-        a = readline()
-        if a != ""
-            push!(arr, a)
-        else
-            break
+function create_F()
+    open("F.txt", "w") do f
+        for i in rand(1:100, rand(5:10))
+            write(f, "$i", "\n")
         end
     end
-    return arr
 end
 
-function write_file(arr)
-    open("labs 4\\F.txt", "w") do f
+function write_file(arr, name)
+    open(name, "w") do f
         for i in arr
             write(f, i, "\n")
         end
     end
 end
 
-function read_file()
-    open("labs 4\\F.txt", "r") do file
+function read_file(name)
+    open(name, "r") do file
         global text
         text = readlines(file)
     end    
     return text
 end
 
-function rewrite()
-    text = read_file()
-    open("labs 4\\G.txt", "w") do file
-        for i in 1:length(text)
-            if i % 2 == 0
-                write(file, text[i], "\n")
-            end
+function rewrite(text)
+    arr = []
+    for i in 1:length(text)
+        if i % 2 == 0
+            push!(arr, text[i])
         end
     end
+    return arr
 end
 
 end
